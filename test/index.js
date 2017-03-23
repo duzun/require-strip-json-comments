e7f49fc9b023fe7207f7f50a3d7fb8b54d4574bc
oscar_busk <oscar.busk@gmail.com>
2017-03-23 23:22:27 +0100

    Better handling of filenames with periods.

--- test/index.js
+++ test/index.js
@@ -5,12 +5,12 @@ var requireJSON = require('..');
 
 
 // 1.
-var config = requireJSON(__dirname + "/config");
+var config = requireJSON(__dirname + "/config.example");
 asert.equal(config.name, 'value', 'yes');
 asert.equal(config.unicorn, 'cake');
 
 // 2.
-var configStr = fs.readFileSync(__dirname + "/config.json", 'utf8');
+var configStr = fs.readFileSync(__dirname + "/config.example.json", 'utf8');
 var config = requireJSON.parse(configStr);
 asert.equal(config['one-line'], 'comment 1');
 asert.equal(config['multi-line'], 'comment 2');
@@ -18,7 +18,7 @@ asert.equal(config['multi-line'], 'comment 2');
 requireJSON.replace();
 
 // 3.
-var config = require("./config");
+var config = require("./config.example");
 asert.equal(config['one-line'], 'comment 1');
 asert.equal(config['multi-line'], 'comment 2');
 
